cf1599b9e334a947bef48fd801b9e742760f6a3f
Michael <mr.babiymichael@gmail.com>
2024-08-02 20:19:19 +0300

    another comments

--- winapp/ConnectedApp/CommonObject.h
+++ winapp/ConnectedApp/CommonObject.h
@@ -20,15 +20,16 @@ struct Brewery
 
 struct CommonObjects
 {
-    std::atomic_bool exit_flag = false;
-    std::atomic_bool start_download = false;
-    std::atomic_bool data_ready = false;
-    std::condition_variable cv;
-    std::mutex mutex;
-    std::vector<Brewery> breweries;
-    Brewery* current_brewery = nullptr; // Pointer to the currently selected brewery
-    std::string current_type = "";
-    std::string current_countries = "";
-    std::string current_search = "";
-    bool reset = false;
+    std::atomic_bool exit_flag = false;     // Flag to indicate when the application should exit
+    std::atomic_bool start_download = false; // Flag to signal the start of a download operation
+    std::atomic_bool data_ready = false;    // Flag to indicate when data is ready to be processed/displayed
+    std::condition_variable cv;             // Condition variable for thread synchronization
+    std::mutex mutex;                       // Mutex to protect shared resources and ensure thread safety
+    std::vector<Brewery> breweries;         // Vector to store brewery data
+    Brewery* current_brewery = nullptr;     // Pointer to the currently selected brewery
+    std::string current_type = "";          // Current selected type of brewery
+    std::string current_countries = "";     // Current selected country or countries
+    std::string current_search = "";        // Current search string
+    bool reset = false;                     // Flag to indicate if a reset operation has been requested
 };
+
